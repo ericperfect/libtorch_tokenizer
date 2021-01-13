0c930007984bb534be9e78673d4d4a6a4872e912
Eric <ericperfectttt@gmail.com>
2021-01-13 16:15:48 +0800

    fix typing

--- test/main.cpp
+++ test/main.cpp
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <memory>
 #include <bits/stdc++.h>
-
 #include "tokenizer.h"
 
 using namespace std;
@@ -25,56 +24,6 @@ string join(const char *a, const char *b)
     message += "/";
     message += b;
     return message;
-}
-
-
-void tokenize(string text, vector<string> &tokens, vector<float> &valid_positions)
-{
-    vector<string> words = basictokenizer.tokenize(text);
-    vector<string> token;
-    vector<string>::iterator itr;
-    for (itr = words.begin(); itr < words.end(); itr++)
-    {
-        token = tokenizer.tokenize(*itr);
-        tokens.insert(tokens.end(), token.begin(), token.end());
-        for (int i = 0; i < token.size(); i++)
-        {
-            if (i == 0)
-                valid_positions.push_back(1);
-            else
-                valid_positions.push_back(0);
-        }
-    }
-}
-
-void encode(string text, vector<float> &input_ids, vector<float> &input_mask, vector<float> &segment_ids,
-            vector<float> &valid_positions)
-{
-    vector<string> tokens;
-    tokenize(text, tokens, valid_positions);
-    if (tokens.size() > max_seq_length - 2)
-    {
-        tokens.assign(tokens.begin(), tokens.begin() + max_seq_length - 2);
-    }
-    // insert "[CLS}"
-    tokens.insert(tokens.begin(), "[CLS]");
-    valid_positions.insert(valid_positions.begin(), 1.0);
-    // insert "[SEP]"
-    tokens.push_back("[SEP]");
-    valid_positions.push_back(1.0);
-    for (int i = 0; i < tokens.size(); i++)
-    {
-        segment_ids.push_back(0.0);
-        input_mask.push_back(1.0);
-    }
-    input_ids = tokenizer.convert_tokens_to_ids(tokens);
-    while (input_ids.size() < max_seq_length)
-    {
-        input_ids.push_back(0.0);
-        input_mask.push_back(0.0);
-        segment_ids.push_back(0.0);
-        valid_positions.push_back(0.0);
-    }
 }
 
 template<typename T>
